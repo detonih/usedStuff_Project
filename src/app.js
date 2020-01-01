b8c3ee02e95bee7289f484b42089d868283881a1
Henrique Detoni <detonihenrique@gmail.com>
2020-01-01 19:16:34 -0300

    add bodyparser

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 
 module.exports = app;
