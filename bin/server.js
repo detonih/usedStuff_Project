23504208ec0bf5b15577ee45327a7365fb9c58b1
Henrique Detoni <detonihenrique@gmail.com>
2020-01-01 18:19:39 -0300

    add app.js

--- bin/server.js
+++ bin/server.js
@@ -1,2 +1,11 @@
-import http from 'http';
+'use strict';
 
+const http = require('http');
+const app = require('../src/app');
+
+
+const port = normalizePort('port', process.env.PORT || '3000');
+app.set('port', port);
+
+//Create server
+const server = http.createServer(app)
