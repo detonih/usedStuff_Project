0025656745772b9c0262aa474a56c13017e7b8be
Henrique Detoni <detonihenrique@gmail.com>
2020-01-02 14:15:29 -0300

    add jwt

--- src/services/hash-password.js
+++ src/services/hash-password.js
@@ -2,22 +2,14 @@
 
 const crypto = require('crypto');
 
-/* 
- * generates random string of characters i.e salt
- * @function
- * @param {number} length - Length of the random string.
- */
+/* generates random string of characters i.e salt */
 const genRandomString = function(length){
     return crypto.randomBytes(Math.ceil(length/2))
             .toString('hex') /** convert to hexadecimal format */
             .slice(0,length);   /** return required number of characters */
 };
 
-/* 
- * hash password with sha512.
- * @function
- * @param {string} password - List of required fields.
- * @param {string} salt - Data to be validated. */
+/* hash password with sha512. */
 
 const sha512 = function(password, salt){
     var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
