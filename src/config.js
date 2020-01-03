d2431428d80c145b64ba8d17282afa0f476beb90
Henrique Detoni <detonihenrique@gmail.com>
2020-01-03 08:56:58 -0300

    change git ignore again

--- src/config.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-global.SALT_KEY = '311b893e4b4c05a59ecd381a89c1af9a'
-
-module.exports = {
-    connectionString: 'mongodb+srv://detonih:cadeirass@clusterusedstuff-yy56t.mongodb.net/test?retryWrites=true&w=majority'
-}
